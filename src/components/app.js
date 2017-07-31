2297a5156773a99d43aec507dd231e5eec9859ba
Anthony Campbell <anthony@cambo.io>
2017-07-31 18:54:40 +1000

    removed content for better boilerplatiness

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Nav from './nav';
-import Footer from './footer';
-import Home from './content/home';
+import Home from './home';
 
 export default class App extends Component {
   render() {
@@ -13,8 +11,6 @@ export default class App extends Component {
                <Route exact path="/" component={Home} />
                <Redirect to="/" />
             </Switch>
-            <Footer />
-            <Nav />
          </div>
       </BrowserRouter>
     );
